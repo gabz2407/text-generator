baed5dc29894fb09f0cd6b61bb0044234b600a7e
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-03-13 20:46:24 +0000

    refactor

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ function showAnswer(response) {
     strings: response.data.answer,
     autoStart: true,
     delay: 20,
+    cursor: "",
   });
 }
 
@@ -11,13 +12,15 @@ function getInputAnswer(event) {
 
   let hiddenElement = document.querySelector(".poem-container");
   hiddenElement.classList.remove("hidden");
+  hiddenElement.innerHTML = `Loading...`;
 
   let inputValue = document.querySelector("#search-input");
   inputValue = inputValue.value;
 
   let prompt = `${inputValue}`;
+  let context = `Add in the end of each answer "<b> AI Text Generator</b>"`;
   let apiKey = "8bc029ce07bb99a925obf42d966t543f";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(showAnswer);
 }
