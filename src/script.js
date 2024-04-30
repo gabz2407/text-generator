6e63e28ecc0675e72018269be6c9d638768fece4
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-30 19:47:12 +0100

    Fixed responsive layout

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ function getInputAnswer(event) {
   inputValue = inputValue.value;
 
   let prompt = `Provide a recipe about ${inputValue}`;
-  let context = `You are a well known food expert, you can show me your skills about world foods. Please answer in this format:<h2>Recipe Name</h2><br /> <h4>Ingredients:</h4><ul><li>Ingredients list</li></ul><h4>Instructions:</h4><p></p>`;
+  let context = `You are a well known food expert, you can show me your skills about world foods. Please answer in this format:<h3>Recipe Name</h3><br /> <h5>Ingredients:</h5><ul><li>Ingredients list</li></ul><h5>Instructions:</h5><p></p>`;
 
   let apiKey = "8bc029ce07bb99a925obf42d966t543f";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
