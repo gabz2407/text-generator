db11905109ff6d4b9bff580dd9e91a40a73ad375
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-06-29 17:29:20 +0100

    JavaScript refactor

--- src/script.js
+++ src/script.js
@@ -30,70 +30,44 @@ function getInputAnswer(event) {
 let searchEngine = document.querySelector("#form-container");
 searchEngine.addEventListener("submit", getInputAnswer);
 
-// out of ideas buttons
-function searchVegan() {
+function searchPrompt(keyword) {
   let recipeBlock = document.querySelector(".recipe-container");
   recipeBlock.classList.remove("hidden");
   recipeBlock.innerHTML = `Wait a second, we are preparing your dish...`;
 
-  let prompt = `Provide a VEGAN recipe`;
+  let prompt = `Provide a ${keyword} recipe`;
   let context = `You are a well known food expert, you can show me your skills about world foods. Please answer in this format:<h3>Recipe Name</h3><br /> <h5>Ingredients:</h5><ul><li>Ingredients list</li></ul><h5>Instructions:</h5><p></p>`;
 
   let apiKey = "8bc029ce07bb99a925obf42d966t543f";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(showAnswer);
+}
+
+// out of ideas buttons
+function searchVegan() {
+  searchPrompt("VEGAN");
 }
 
 let vegan = document.querySelector(".vegan");
 vegan.addEventListener("click", searchVegan);
 
 function searchVegetarian() {
-  let recipeBlock = document.querySelector(".recipe-container");
-  recipeBlock.classList.remove("hidden");
-  recipeBlock.innerHTML = `Wait a second, we are preparing your dish...`;
-
-  let prompt = `Provide a VEGETARIAN recipe`;
-  let context = `You are a well known food expert, you can show me your skills about world foods. Please answer in this format:<h3>Recipe Name</h3><br /> <h5>Ingredients:</h5><ul><li>Ingredients list</li></ul><h5>Instructions:</h5><p></p>`;
-
-  let apiKey = "8bc029ce07bb99a925obf42d966t543f";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(showAnswer);
+  searchPrompt("VEGETARIAN");
 }
 
 let vegetarian = document.querySelector(".vegetarian");
 vegetarian.addEventListener("click", searchVegetarian);
 
 function searchCarnivore() {
-  let recipeBlock = document.querySelector(".recipe-container");
-  recipeBlock.classList.remove("hidden");
-  recipeBlock.innerHTML = `Wait a second, we are preparing your dish...`;
-
-  let prompt = `Provide a CARNIVORE recipe`;
-  let context = `You are a well known food expert, you can show me your skills about world foods. Please answer in this format:<h3>Recipe Name</h3><br /> <h5>Ingredients:</h5><ul><li>Ingredients list</li></ul><h5>Instructions:</h5><p></p>`;
-
-  let apiKey = "8bc029ce07bb99a925obf42d966t543f";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(showAnswer);
+  searchPrompt("CARNIVORE");
 }
 
 let carnivore = document.querySelector(".carnivore");
 carnivore.addEventListener("click", searchCarnivore);
 
 function searchPescatarian() {
-  let recipeBlock = document.querySelector(".recipe-container");
-  recipeBlock.classList.remove("hidden");
-  recipeBlock.innerHTML = `Wait a second, we are preparing your dish...`;
-
-  let prompt = `Provide a PESCATARIAN recipe`;
-  let context = `You are a well known food expert, you can show me your skills about world foods. Please answer in this format:<h3>Recipe Name</h3><br /> <h5>Ingredients:</h5><ul><li>Ingredients list</li></ul><h5>Instructions:</h5><p></p>`;
-
-  let apiKey = "8bc029ce07bb99a925obf42d966t543f";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(showAnswer);
+  searchPrompt("PESCATARIAN");
 }
 
 let pescatarian = document.querySelector(".pescatarian");
